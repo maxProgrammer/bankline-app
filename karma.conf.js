91cdb50891b0d4076238c9b631fbdfda8cf41a06
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-07 23:14:05 -0300

    setado aplicação frontend para consumir backend no heroku

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bankline'),
+      dir: require('path').join(__dirname, './coverage/bankline-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
